2bdb1a4616e399af12fcdf6f1de3626c4bf8de12
Andrew Shay <andrewshay@shayconcepts.com>
2015-08-19 20:41:15 -0400

    Hides console

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 import py2exe
 setup(
     # Change console to window or windows?
-    console = [
+    windows = [
         {
             "script": "laziitv.pyw",
             "icon_resources": [(0, "D:\Andrew\Python Workspace\LaziiTV\Source\icon_big.ico")]

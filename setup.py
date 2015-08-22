9cd4dfabdb231dc19cf85fb58eb0cd550fbdb574
Andrew Shay <andrewshay@shayconcepts.com>
2015-08-22 13:30:44 -0400

    icon location read from current directory

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from distutils.core import setup
 import py2exe
+
 setup(
     # Change console to window or windows?
-    windows = [
+    windows=[
         {
             "script": "laziitv.pyw",
-            "icon_resources": [(0, "D:\Andrew\Python Workspace\LaziiTV\Source\icon_big.ico")]
+            "icon_resources": [(0, "icon_big.ico")]
         }
     ],
 )
